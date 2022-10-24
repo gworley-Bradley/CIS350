385e9fe4cebbb8ef3d41b7af2224f5ddbbc30216
gworley-Bradley <81546469+gworley-Bradley@users.noreply.github.com>
2022-10-24 02:23:01 -0500

    Added Assignment5B

---
+++ Assignment5B/Assets/MyFirstPersonPlayer/Scripts/CoinPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    private UIManager uiManager;
+
+    private void Start()
+    {
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            uiManager.coins++;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assignment5B/Assets/MyFirstPersonPlayer/Scripts/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] hiddenExits;
+    public Text scoreText;
+    public int skeletons = 0;
+    public int coins = 0;
+
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!won)
+        {
+            scoreText.text = "Coins: " + coins + "\n" + "Skeletons: " + skeletons;
+        }
+        if (won)
+        {
+            scoreText.text = "You Win!" + "\n" + "Press R to Try Again!";
+        }
+
+        if (won && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+
+        if (coins == 5 && skeletons == 10)
+        {
+            hiddenExits = GameObject.FindGameObjectsWithTag("Final");
+            for (int i = hiddenExits.Length - 1; i >= 0; i--)
+            {
+                Destroy(hiddenExits[i].gameObject);
+            }
+        }
+
+        
+    }
+}
